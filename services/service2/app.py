3390f11db2f6fc147e19f7dd21a7e5c6fe1dec5c
nekar5 <70347988+nekar5@users.noreply.github.com>
2023-03-07 23:22:16 +0200

    Add files via upload

---
+++ services/service2/app.py
@@ -0,0 +1,89 @@
+from flask import Flask, jsonify, request, g
+import sqlite3
+from flower import Flower
+
+app = Flask('service2')
+
+def get_db_connection():
+    conn = getattr(g,'_database',None)
+    if conn is None:
+        conn = g._database = sqlite3.connect('flowers.db')
+    return conn
+
+@app.route('/')
+def start_point():
+    return "Start service 2 for flowers"
+
+@app.route('/flowers/getbyid', methods=['GET'])
+def get_flower_by_id():
+    """
+    :id (str): flower id
+    :return 1: flower info (json)
+    :return 2: error message (json)
+    """
+    args = request.args
+    flower_id = args.to_dict()['flowerId']
+    db = get_db_connection()
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM flower WHERE id=?", (flower_id,))
+    flower = cursor.fetchall()
+    if flower:
+        flowers = [Flower(*row) for row in flower]
+        flowers = [vars(flower) for flower in flowers]
+        return jsonify(flower), 200
+    else:
+        return jsonify({'error': 'Flower not found'}), 404
+
+@app.route('/flowers/getbyname', methods=['GET'])
+def get_flower_by_name():
+    """
+    :name (str): flower name
+    :return 1: flower info (json)
+    :return 2: error message (json)
+    """
+    args = request.args
+    flowerName = args.to_dict()['flowerName']
+    db = get_db_connection()
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM flower WHERE name=?", (flowerName,))
+    flower = cursor.fetchall()
+    if flower:
+        flowers = [Flower(*row) for row in flower]
+        flowers = [vars(flower) for flower in flowers]
+        return jsonify(flower), 200
+    else:
+        return jsonify({'error': 'Flower not found'}), 404
+
+@app.route('/flowers/add', methods=['POST'])
+def add_flower():
+    """
+    :flower (dict): flower (id, name, color, climate, price)
+    :return 1: error message (json)
+    :return 2: success message (json)
+    """
+    flower = request.get_json()
+    db = get_db_connection()
+    cursor = db.cursor()
+    if not flower or 'id' not in flower or 'name' not in flower or 'price'<=0:
+        return jsonify({'error': 'Invalid request'}), 400
+    flower = Flower(flower['id'], flower['name'], flower['color'], flower['climate'], flower['price'])
+    cursor.execute('INSERT INTO flower (id, name, color, climate, price) VALUES (?, ?, ?, ?, ?)', (flower.id, flower.name, flower.color, flower.climate, flower.price))
+    db.commit()
+    
+    return jsonify({'message': 'Flower added successfully'}), 200
+
+@app.route('/flowers/getall', methods=['GET'])
+def get_all_flowers():
+    """
+    :return: all flowers (json)
+    """
+    db = get_db_connection()
+    cursor = db.cursor()
+    cursor.execute('SELECT * FROM flower')
+    rows = cursor.fetchall()
+    flowers = [Flower(*row) for row in rows]
+    flowers = [vars(flower) for flower in flowers]
+    return jsonify(flowers), 200
+
+if __name__ == '__main__':
+    app.run(port=8080)
---
+++ services/service2/flower.py
@@ -0,0 +1,7 @@
+class Flower:
+    def __init__(self, id, name, color, climate, price):
+        self.id = id
+        self.name = name
+        self.color = color
+        self.climate = climate
+        self.price = price
