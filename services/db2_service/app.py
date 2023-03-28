fcedddd537931f5e61d197061e8de13758a3ce6d
nekar5 <70347988+nekar5@users.noreply.github.com>
2023-03-28 21:01:22 +0300

    Update app.py

--- services/db2_service/app.py
+++ services/db2_service/app.py
@@ -1,9 +1,6 @@
-import pathlib
 from flask import Flask, jsonify, request, g
 import sqlite3
 from flower import Flower
-from pathlib import Path
-
 
 app = Flask('service2')
 
@@ -41,7 +38,7 @@ def get_flower_by_id():
 def get_flower_by_name():
     """
     :name (str): flower name
-    :return 1: flower info (json)
+    :return 1: flower data (json)
     :return 2: error message (json)
     """
     args = request.args
@@ -60,7 +57,7 @@ def get_flower_by_name():
 @app.route('/flowers/add', methods=['POST'])
 def add_flower():
     """
-    :flower (dict): flower (id, name, color, climate, price)
+    :flower (dict): flower (id, name, color, climate, price, image)
     :return 1: error message (json)
     :return 2: success message (json)
     """
@@ -70,7 +67,7 @@ def add_flower():
     if not flower or 'id' not in flower or 'name' not in flower or 'price'<=0:
         return jsonify({'error': 'Invalid request'}), 400
     flower = Flower(flower['id'], flower['name'], flower['color'], flower['climate'], flower['price'], flower['image'])
-    cursor.execute('INSERT INTO flower (id, name, color, climate, price) VALUES (?, ?, ?, ?, ?)', (flower.id, flower.name, flower.color, flower.climate, flower.price, flower.image))
+    cursor.execute('INSERT INTO flower (id, name, color, climate, price) VALUES (?, ?, ?, ?, ?, ?)', (flower.id, flower.name, flower.color, flower.climate, flower.price, flower.image))
     db.commit()
     
     return jsonify({'message': 'Flower added successfully'}), 200
@@ -87,29 +84,6 @@ def get_all_flowers():
     flowers = [Flower(*row) for row in rows]
     flowers = [vars(flower) for flower in flowers]
     return jsonify(flowers), 200
-
-@app.route('/flowers/addimages', methods=['GET']) # POST?
-def add_images():
-    """
-    adds image paths to database if image name matches flower name
-    :returns: success message (json)
-    """
-    db = get_db_connection()
-    cursor = db.cursor()
-
-    #source = request.get_json() ?
-    source='C:\\Users\\Nestor\\Desktop\\flowers' # path to folder with images (Name.jpg)
-    """
-    wExt=os.listdir(source)
-    noExt=[x.split('.')[0] for x in l]
-    """
-    namepaths = []
-    for filepath in pathlib.Path(source).glob('**/*'):
-        namepaths.append(filepath.absolute())
-    for path in namepaths:
-        cursor.execute('update flower set image="'+str(path)+'" where name="'+Path(path).stem+'"')
-        db.commit()
-    return jsonify('images added'), 200
 
 if __name__ == '__main__':
     app.run(port=8080)
